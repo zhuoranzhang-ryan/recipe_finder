a0d1cbd40e9f24a80e3c50a2b7ede89edb035b96
Kana Moore <dudiw.kana@hotmail.co.jp>
2020-05-17 21:01:22 -0500

    Completed scrape.py

---
+++ scrape_image.py
@@ -0,0 +1,22 @@
+
+from bs4 import BeautifulSoup
+from splinter import Browser
+import time
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver"}
+    return Browser("chrome", **executable_path, headless=True)
+
+def scrape_image(recipe_id):
+    browser = init_browser()
+    website_url = f"https://www.food.com/recipe-{recipe_id}"
+    browser.visit(website_url)
+    website_html = browser.html
+    website_soup = BeautifulSoup(website_html, "html.parser")
+    website_results = website_soup.find("div", class_="inner-wrapper")
+    picture_url = website_results.img["data-src"]
+    browser.quit()
+    print(picture_url)
+    return picture_url
+
+scrape_image(39087)
